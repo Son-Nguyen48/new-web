4a7c19b07408dedb6f2f2e8fe36146f676bd0c68
Son-Nguyen48 <theson4895@gmail.com>
2021-11-09 01:33:24 +0700

    4th

--- first/script.js
+++ first/script.js
@@ -7,14 +7,39 @@ $(document).ready(function(){
         else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
     
+    // slide up script
+    
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
 
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        string: ["Frontend Developer","Gamer","Read book","Watch movie"],
+        typedSpeed: 40,
+        backSpeed: 60,
+        loop: true
+    })
+    var typed = new Typed(".typing-2", {
+        string: ["Frontend Developer","Gamer","Read book","Watch movie"],
+        typedSpeed: 40,
+        backSpeed: 60,
+        loop: true
+    })
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
